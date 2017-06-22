f18b46a3539868773e406fc1d4759ad03a026909
xiplus <huangxuanyuxiplus@gmail.com>
2017-06-22 20:20:55 +0800

    sample config fix

--- config/config.example.php
+++ config/config.example.php
@@ -11,16 +11,18 @@ $C['DBuser'] = 'user';
 $C['DBpass'] = 'pass';
 $C['DBTBprefix'] = 'shiritori_';
 
-$C['ScoreStart'] = 20;
-$C['ScoreNotfound'] = -3;
-$C['ScoreTip'] = -10;
-$C['ScoreRepeat'] = -5;
-$C['ScoreAnswer'] = 1;
+$C['Rule']['Start'] = 20;
+$C['Rule']['Notfound'] = -3;
+$C['Rule']['Tip'] = -10;
+$C['Rule']['Repeat'] = -5;
+$C['Rule']['Answer'] = 1;
 
 $C['LogKeep'] = 86400*7;
 
 $C["allowsapi"] = array("cli");
 
 $C['Module']['TW-MOE-Dict'] = __DIR__."/../function/TW-MOE-Dict/dict.php";
+
+$C['DataPath'] = __DIR__."/../data/";
 
 $G["db"] = new PDO ('mysql:host='.$C["DBhost"].';dbname='.$C["DBname"].';charset=utf8', $C["DBuser"], $C["DBpass"]);
